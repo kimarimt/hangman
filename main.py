544e8c9c7d29a43a1fa1ebb8cad32a601f38b060
Alec German <kimarimt@protonmail.com>
2022-05-14 11:26:46 -0400

    Added basic game structure

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import os
+import time
+
+
+def clear():
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main():
-    print('Hello, World!')
+    word = 'hello'
+    dashes = ['_' for _ in range(len(word))]
+    guesses = set()
+
+    while ''.join(dashes) != word:
+        guess = input('Guess a letter: ')
+
+        if guess not in guesses:
+            if guess in word:
+                for position, letter in enumerate(word):
+                    if guess == letter:
+                        dashes[position] = letter
+            else:
+                print('Bad guess')
+        else:
+            print(f'You already guessed {guess}')
+
+        clear()
+        guesses.add(guess)
+        print(f'Word: {"".join(dashes)}')
+        print(f'Guesses: {", ".join(guesses)}')
 
 
 if __name__ == '__main__':
