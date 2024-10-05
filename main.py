0753b121210cac567f682d593a945a17291dd44a
Alec German <kimarimt@protonmail.com>
2024-10-05 17:17:48 -0400

    added game images and a clear function

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+import assets
+import helpers
+
+
 def main():
     word = 'hello'
     dashes = ['_' for _ in word]
     guesses = set()
-    lives = 6
+    lives = len(assets.STAGES) - 1
     final_word = ''.join(dashes)
 
-    while final_word != word and lives > 0:
-        print(f'Word: {final_word}')
+    print(assets.LOGO)
+    input('Press any key to continue: ')
+    helpers.clear()
+
+    while final_word != word and lives != 0:
+        print(assets.STAGES[lives])
+        print(f'\nWord: {final_word}')
         print(f'Guesses: {", ".join(guesses)}')
         guess = input('Guess a letter: ')
 
         if len(guess) != 1:
             print('Guesses should be one letter')
+            helpers.clear()
             continue
         elif guess in word:
             for i, ch in enumerate(word):
@@ -23,8 +33,10 @@ def main():
             lives -= 1
 
         guesses.add(guess)
+        helpers.clear()
     else:
         if lives == 0:
+            print(assets.STAGES[lives])
             print('GAME OVER!', end=' ')
         else:
             print('YOU WIN!', end=' ')
